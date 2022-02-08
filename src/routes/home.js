782adad9c28c3498188ef396520712f76b4b4883
Harshil-Modi <hmodi562000@gmail.com>
2022-02-08 23:10:14 +0530

    Improved project details

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,5 +1,5 @@
 import { sendResponseHtml } from "../lib/responses";
 
 export const home = () => {
-   return sendResponseHtml('A private service that shortens an URL, and redirects to the original URL.');
+   return sendResponseHtml('A service for customizing shared URLs.');
 }
